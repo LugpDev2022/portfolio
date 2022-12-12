4b095690e30df7816cd48250e0a4a81359e2c42a
LugpDev2022 <lugpdev2022@gmail.com>
2022-12-12 16:11:26 -0600

    Add bootstrap using react-bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 
 function App() {
-  return <h1>Portfolio</h1>;
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1>Portfolio</h1>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default App;
