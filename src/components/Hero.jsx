20f4fac0fe877ec1ed34ab16bc8cf6f1fa0cd8e9
LugpDev2022 <lugpdev2022@gmail.com>
2022-12-13 10:44:04 -0600

    Set the correct bg on the hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,25 +1,39 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Col from "react-bootstrap/Col";
+import bg from "../assets/pc-bg.webp";
 
-const Hero = () => (
-  <header className="row py-5 bg-secondary">
-    <Col xs={12} className="text-center mb-4">
-      <img
-        src="http://t0.gstatic.com/licensed-image?q=tbn:ANd9GcSe7vU6RlwDwFL44J6xtBdRmmqhQRg4FQYYKi0VwxV7h1v5Erv1-jiqfGxHEG9e9b8y2IfQ7INlB_IjzUMT-yg"
-        alt="profile image"
-        className="my-img"
-      />
-    </Col>
-    <Col xs={12} className="mt-3">
-      <h1 className="title">
+const Hero = () => {
+  const [bgHeight, setBgHeight] = useState(0);
+  const header = useRef();
+
+  useEffect(() => {
+    setBgHeight(header.current.offsetHeight);
+  }, [header]);
+
+  return (
+    //TODO: Change the profile image
+    <header ref={header} className="row">
+      <Col xs={12} className="text-center mb-4 mt-5">
+        <img
+          src="http://t0.gstatic.com/licensed-image?q=tbn:ANd9GcSe7vU6RlwDwFL44J6xtBdRmmqhQRg4FQYYKi0VwxV7h1v5Erv1-jiqfGxHEG9e9b8y2IfQ7INlB_IjzUMT-yg"
+          alt="profile image"
+          className="my-img"
+        />
+      </Col>
+      <h1 className="title col-12 mt-3">
         <span>Luis Alberto&nbsp;</span>
         <span>Cervantes García</span>
       </h1>
-    </Col>
-    <Col xs={12} className="mt-1">
-      <h2 className="subtitle">Frontend Developer</h2>
-    </Col>
-  </header>
-);
+      <h2 className="subtitle col-12 mt-1 mb-5">Frontend Developer</h2>
+
+      <img
+        style={{ height: `${bgHeight}px` }}
+        src={bg}
+        alt="background"
+        className="pc-bg"
+      />
+    </header>
+  );
+};
 
 export default Hero;
