ca258c6f2aa9bbbe4a1a65130a253167f8d189e7
LugpDev2022 <lugpdev2022@gmail.com>
2022-12-14 11:29:47 -0600

    Create an array with the projects info

---
+++ src/assets/projects.js
@@ -0,0 +1,30 @@
+import TimeAppSS from "./TimeAppSS.webp";
+import CalculatorAppSS from "./CalculatorAppSS.webp";
+import NotFoundPage from "./404PageSS.webp";
+import TodoAppSS from "./TodoAppSS.webp";
+import AdviceGeneratorSS from "./AdviceGeneratorSS.webp";
+
+const projects = [
+  {
+    title: "Time App",
+    url: TimeAppSS,
+  },
+  {
+    title: "Calculator App",
+    url: CalculatorAppSS,
+  },
+  {
+    title: "404 Page",
+    url: NotFoundPage,
+  },
+  {
+    title: "Todo App",
+    url: TodoAppSS,
+  },
+  {
+    title: "Advice Generator",
+    url: AdviceGeneratorSS,
+  },
+];
+
+export default projects;
