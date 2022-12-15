949a80dd5460bfad49248eac872eb2b7efd4036c
LugpDev2022 <lugpdev2022@gmail.com>
2022-12-14 19:53:14 -0600

    Add new features to the project modal

--- src/components/ProjectModal.jsx
+++ src/components/ProjectModal.jsx
@@ -1,11 +1,14 @@
 import { useContext } from "react";
 import Modal from "react-bootstrap/Modal";
 import { AppContext } from "../context";
+import { ListGroup } from "react-bootstrap";
+import { AiFillGithub, AiOutlineLink } from "react-icons/ai";
 
+//TODO: Split techStack into 2 columns
 export const ProjectModal = () => {
   const { projectModalInfo, showProjectModal, handleCloseProjectModal } =
     useContext(AppContext);
-  const { title, imgSrc } = projectModalInfo;
+  const { title, imgSrc, techStack, githubRepo, url } = projectModalInfo;
 
   return (
     <Modal
@@ -14,13 +17,40 @@ export const ProjectModal = () => {
       centered
       className="custom-modal"
       size="lg"
+      scrollable
       backdrop={false}
     >
-      <Modal.Header closeButton closeVariant="white" style={{ border: "none" }}>
-        <Modal.Title>{title}</Modal.Title>
+      <Modal.Header
+        closeButton
+        closeVariant="white"
+        style={{ border: "none" }}
+        className="d-flex align-items-center justify-content-start gap-3"
+      >
+        <Modal.Title className="fw-bold">{title}</Modal.Title>
+        <a href={githubRepo} target="_blank">
+          <AiFillGithub />
+        </a>
+        <a href={url} target="_blank">
+          <AiOutlineLink />
+        </a>
       </Modal.Header>
       <Modal.Body>
         <img src={imgSrc} alt={title} className="img-fluid" />
+        <h5 className="mt-3 h3">TechStack</h5>
+        <ListGroup variant="flush">
+          {techStack &&
+            techStack.map((tech) => (
+              <ListGroup.Item
+                key={tech}
+                style={{
+                  color: "white",
+                  border: "none",
+                }}
+              >
+                {tech}
+              </ListGroup.Item>
+            ))}
+        </ListGroup>
       </Modal.Body>
     </Modal>
   );
