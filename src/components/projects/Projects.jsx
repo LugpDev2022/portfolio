5d68abd520917d6ff2ac63deecbf34281f0f74b0
LugpDev2022 <lugpdev2022@gmail.com>
2022-12-15 12:12:50 -0600

    Improve the semantic meaning of the projects structure

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -3,25 +3,30 @@ import Row from "react-bootstrap/Row";
 import { AppContext } from "../../context";
 import { ProjectCard, ProjectModal } from "./";
 import projects from "../../assets/projects";
+import { Col } from "react-bootstrap";
 
 export const Projects = () => {
   const { isNavFixed } = useContext(AppContext);
 
   return (
     <>
-      <Row id="projects">
-        <h3
-          style={{ marginTop: isNavFixed ? "142px" : "80px" }}
-          className="text-center subtitle animate__animated animate__fadeIn"
-        >
-          PROJECTS
-        </h3>
-      </Row>
+      <Row as="main" id="projects">
+        <Col xs={12}>
+          <h3
+            style={{ marginTop: isNavFixed ? "142px" : "80px" }}
+            className="subtitle animate__animated animate__fadeIn"
+          >
+            PROJECTS
+          </h3>
+        </Col>
 
-      <Row className="justify-content-around mt-5 px-5">
-        {projects.map((project) => {
-          return <ProjectCard project={project} key={project.title} />;
-        })}
+        <Col xs={12} className="mt-5 px-5">
+          <Row className="justify-content-around">
+            {projects.map((project) => {
+              return <ProjectCard project={project} key={project.title} />;
+            })}
+          </Row>
+        </Col>
       </Row>
 
       <ProjectModal />
