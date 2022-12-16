5d7b68cbf2dba449f76e38642c699e96ed1de50b
LugpDev2022 <lugpdev2022@gmail.com>
2022-12-16 14:39:26 -0600

    Add the tooltips to the skillset

--- src/components/Skillset.jsx
+++ src/components/Skillset.jsx
@@ -1,4 +1,4 @@
-import { Col, Row } from "react-bootstrap";
+import { Col, Container, OverlayTrigger, Row, Tooltip } from "react-bootstrap";
 import { AiFillHtml5 } from "react-icons/ai";
 import {
   FaCss3Alt,
@@ -11,19 +11,60 @@ import { SiJavascript, SiJest } from "react-icons/si";
 
 export const Skillset = () => {
   return (
-    <Row className="bg-secondary mt-3" id="skills">
+    <Row className="bg-secondary my-3" id="skills">
       <Col xs={12}>
-        <h3 className="subtitle mt-5">SKILLSET</h3>
+        <h3 className="subtitle my-5">SKILLSET</h3>
       </Col>
-      <Col xs={12}>
-        <AiFillHtml5 className="skillset-icon" />
-        <FaCss3Alt className="skillset-icon" />
-        <FaSass className="skillset-icon" />
-        <FaBootstrap className="skillset-icon" />
-        <SiJavascript className="skillset-icon" />
-        <FaReact className="skillset-icon" />
-        <SiJest className="skillset-icon" />
-        <FaGitAlt className="skillset-icon" />
+      <Col xs={12} className="d-flex">
+        <OverlayTrigger overlay={<Tooltip id="tooltip-html">HTML</Tooltip>}>
+          <div>
+            <AiFillHtml5 className="skillset-icon" />
+          </div>
+        </OverlayTrigger>
+
+        <OverlayTrigger overlay={<Tooltip id="tooltip-css">CSS</Tooltip>}>
+          <div>
+            <FaCss3Alt className="skillset-icon" />
+          </div>
+        </OverlayTrigger>
+        <OverlayTrigger overlay={<Tooltip id="tooltip-sass">Sass</Tooltip>}>
+          <div>
+            <FaSass className="skillset-icon" />
+          </div>
+        </OverlayTrigger>
+        <OverlayTrigger
+          overlay={<Tooltip id="tooltip-bootstrap">Bootstrap 5</Tooltip>}
+        >
+          <div>
+            <FaBootstrap className="skillset-icon" />
+          </div>
+        </OverlayTrigger>
+
+        <OverlayTrigger
+          overlay={<Tooltip id="tooltip-javascript">JavaScript</Tooltip>}
+        >
+          <div>
+            <SiJavascript className="skillset-icon" />
+          </div>
+        </OverlayTrigger>
+
+        <OverlayTrigger overlay={<Tooltip id="tooltip-react">React</Tooltip>}>
+          <div>
+            <FaReact className="skillset-icon" />
+          </div>
+        </OverlayTrigger>
+
+        <OverlayTrigger overlay={<Tooltip id="tooltip-jest">Jest</Tooltip>}>
+          <div>
+            <SiJest className="skillset-icon" />
+          </div>
+        </OverlayTrigger>
+
+        <OverlayTrigger overlay={<Tooltip id="tooltip-git">Git</Tooltip>}>
+          <div>
+            <FaGitAlt className="skillset-icon" />
+          </div>
+        </OverlayTrigger>
       </Col>
     </Row>
   );
