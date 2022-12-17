0a44ff8fde980012bbb218d804615c240db54e55
LugpDev2022 <lugpdev2022@gmail.com>
2022-12-17 15:51:45 -0600

    Refactor on ProjectModal test

--- tests/components/project/ProjectModal.test.jsx
+++ tests/components/project/ProjectModal.test.jsx
@@ -5,37 +5,70 @@ import { useProjectModal } from "../../../src/hooks/useProjectModal";
 vi.mock("../../../src/hooks/useProjectModal");
 
 describe("tests on <ProjectModal />", () => {
-  test("should show the modal correctly", () => {
+  beforeEach(() => vi.clearAllMocks());
+
+  test("should show the title", () => {
     const title = "Testing Portfolio";
+
+    useProjectModal.mockReturnValue({
+      showProjectModal: true,
+      title,
+    });
+
+    render(<ProjectModal />);
+    expect(screen.getByText(title)).toBeTruthy();
+  });
+
+  test("should show the github link", () => {
     const githubRepo = "https://github.com/portfolio";
+
+    useProjectModal.mockReturnValue({
+      showProjectModal: true,
+      githubRepo,
+    });
+
+    render(<ProjectModal />);
+    const githubLink = screen.getByTestId("githubRepoLink");
+    expect(githubLink.href).toBe(githubRepo);
+  });
+
+  test("should show the web link", () => {
     const url = "https://lcervant.tech/";
+
+    useProjectModal.mockReturnValue({
+      showProjectModal: true,
+      url,
+    });
+
+    render(<ProjectModal />);
+    const urlLink = screen.getByTestId("urlLink");
+    expect(urlLink.href).toBe(url);
+  });
+
+  test("should show the web image", () => {
     const imgSrc = "https://portfolio/portfolio.png";
+
+    useProjectModal.mockReturnValue({
+      showProjectModal: true,
+      imgSrc,
+    });
+
+    render(<ProjectModal />);
+    const webImage = screen.getByRole("img");
+    expect(webImage.src).toBe(imgSrc);
+  });
+
+  test("should show the technologies correctly", () => {
     const techStackFirstPart = ["React", "Vitest"];
     const techStackSecondPart = ["JS", "CSS"];
 
     useProjectModal.mockReturnValue({
       showProjectModal: true,
-      title,
-      githubRepo,
-      url,
-      imgSrc,
       techStackFirstPart,
       techStackSecondPart,
     });
 
     render(<ProjectModal />);
-
-    expect(screen.getByText(title)).toBeTruthy();
-
-    const githubLink = screen.getByTestId("githubRepoLink");
-    expect(githubLink.href).toBe(githubRepo);
-
-    const urlLink = screen.getByTestId("urlLink");
-    expect(urlLink.href).toBe(url);
-
-    const webImage = screen.getByRole("img");
-    expect(webImage.src).toBe(imgSrc);
-
     expect(screen.getByText("Tech Stack")).toBeTruthy();
 
     techStackFirstPart.map((tech) => {
