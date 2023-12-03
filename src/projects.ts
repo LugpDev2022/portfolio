23f3440245cc314c28daf8d27cb1ca1e35e6935b
LugpDev2022 <lugpdev2022@gmail.com>
2023-12-02 22:13:39 -0600

    Create <Project />

---
+++ src/projects.ts
@@ -0,0 +1,46 @@
+export interface Project {
+  title: string;
+  image: string;
+  description: string;
+  techStack: string[];
+  url: string;
+}
+
+export const projects: Project[] = [
+  {
+    title: 'Todo App',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/9/99/White_Background_%28To_id_screen_dust_during_cleanup%29.jpg',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tincidunt consectetur nisi id vulputate. Nunc ultricies placerat augue, ',
+    techStack: ['React', 'Bootstrap'],
+    url: 'https://lugp-todo-app.netlify.app',
+  },
+  {
+    title: 'Todo App',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/9/99/White_Background_%28To_id_screen_dust_during_cleanup%29.jpg',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tincidunt consectetur nisi id vulputate. Nunc ultricies placerat augue, ',
+    techStack: ['React', 'Bootstrap'],
+    url: 'https://lugp-todo-app.netlify.app',
+  },
+  {
+    title: 'Todo App',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/9/99/White_Background_%28To_id_screen_dust_during_cleanup%29.jpg',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tincidunt consectetur nisi id vulputate. Nunc ultricies placerat augue, ',
+    techStack: ['React', 'Bootstrap'],
+    url: 'https://lugp-todo-app.netlify.app',
+  },
+  {
+    title: 'Todo App',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/9/99/White_Background_%28To_id_screen_dust_during_cleanup%29.jpg',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tincidunt consectetur nisi id vulputate. Nunc ultricies placerat augue, ',
+    techStack: ['React', 'Bootstrap'],
+    url: 'https://lugp-todo-app.netlify.app',
+  },
+];
