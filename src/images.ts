1a461cd5cbdaf7b8c42a66e5c6e27fdd04fd2f76
LugpDev2022 <lugpdev2022@gmail.com>
2023-12-02 23:35:34 -0600

    Add skillset section

---
+++ src/images.ts
@@ -0,0 +1,18 @@
+export const languages: string[] = [
+  'html',
+  'javascript',
+  'css',
+  'typescript',
+  'sass',
+];
+
+export const frameworks: string[] = [
+  'react',
+  'nextjs',
+  'astro',
+  'tailwind',
+  'bootstrap',
+  'jest',
+];
+
+export const tools: string[] = ['vercel', 'vite', 'netlify', 'git', 'firebase'];
