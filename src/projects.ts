4949a7acdccbdb17934a7b57c4adbb010f12d02a
LugpDev2022 <lugpdev2022@gmail.com>
2023-12-08 17:57:05 -0600

    Add missing projects info

--- src/projects.ts
+++ src/projects.ts
@@ -10,7 +10,8 @@ export const projects: Project[] = [
   {
     title: 'Todo App',
     image: '/images/projects/todoApp.png',
-    description: '',
+    description:
+      'Web application designed for organizing your daily tasks effectively.',
     techStack: ['React', 'Bootstrap'],
     url: 'https://lugp-todo-app.netlify.app',
   },
@@ -25,9 +26,10 @@ export const projects: Project[] = [
   {
     title: 'Advice Generator',
     image: '/images/projects/adviceGenerator.png',
-    description: '',
+    description:
+      "Access the finest advice available with this web application, catering to everyone's needs.",
     techStack: ['React', 'Bootstrap'],
-    url: 'https://lugp-todo-app.netlify.app',
+    url: 'https://advice-generator-lugpdev2022.netlify.app/',
   },
   {
     title: 'Time App',
@@ -35,21 +37,20 @@ export const projects: Project[] = [
     description:
       'Small app for basic time tasks like watching the hour and creating timers.',
     techStack: ['React', 'Bootstrap', 'Redux', 'Jest'],
-    url: 'https://lugp-todo-app.netlify.app',
+    url: 'https://time-app-lugpdev2022.netlify.app/',
   },
   {
     title: 'News Page',
     image: '/images/projects/news-page.png',
-    description: '',
+    description: 'Homepage of a news website.',
     techStack: ['Astro', 'Bootstrap', 'Sass'],
-    url: 'https://lugp-todo-app.netlify.app',
+    url: 'https://news-page-lugpdev2022.netlify.app/',
   },
   {
     title: '404 Not Found',
     image: '/images/projects/not-found.png',
-    description:
-      'Small app for basic time tasks like watching the hour and creating timers.',
+    description: 'Typical 404 Not Found page.',
     techStack: ['Astro', 'Tailwind', 'Css'],
-    url: 'https://lugp-todo-app.netlify.app',
+    url: 'https://lugp-not-found.netlify.app/',
   },
 ];
