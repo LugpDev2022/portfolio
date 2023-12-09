9f87d283c45aa72a79e82197774b64f103f0927f
LugpDev2022 <lugpdev2022@gmail.com>
2023-12-08 18:26:15 -0600

    Add vercel speed insights

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
-import react from "@astrojs/react";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   server: {
-    port: 3000
+    port: 3000,
   },
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  output: 'server',
+  adapter: vercel({
+    speedInsights: {
+      enabled: true,
+    },
+  }),
 });
