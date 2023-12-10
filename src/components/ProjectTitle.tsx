69a9e829bf99cc4cf126f8c46689acf2a0ada1e7
LugpDev2022 <lugpdev2022@gmail.com>
2023-12-10 14:03:50 -0600

    Replace Project.tsx with Project.astro

---
+++ src/components/ProjectTitle.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+
+interface Props {
+  children: React.ReactNode;
+  url: string;
+}
+
+const ProjectTitle: React.FC<Props> = ({ children, url }) => {
+  const MOBILE_BREAKPOINT = 640;
+
+  const [isMobile, setIsMobile] = useState<boolean>(
+    window.innerWidth < MOBILE_BREAKPOINT
+  );
+
+  const className = 'inline-flex gap-[10px] project-title';
+
+  useEffect(() => {
+    const handler = () => {
+      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+    };
+
+    window.addEventListener('resize', handler);
+
+    return () => {
+      window.removeEventListener('resize', handler);
+    };
+  }, []);
+
+  return isMobile ? (
+    <a href={url} rel='noreferrer' target='_blank' className={className}>
+      {children}
+    </a>
+  ) : (
+    <h4 className={className}>{children}</h4>
+  );
+};
+
+export default ProjectTitle;
