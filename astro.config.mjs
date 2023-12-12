f557aad2811fc877b365af5455145a47a5e6fee4
LugpDev2022 <lugpdev2022@gmail.com>
2023-12-11 20:56:24 -0600

    Start translating page to spanish

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 
-import react from "@astrojs/react";
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+
+import astroI18next from 'astro-i18next';
 
 // https://astro.build/config
 export default defineConfig({
   server: {
-    port: 3000
+    port: 3000,
   },
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react(), astroI18next()],
 });
