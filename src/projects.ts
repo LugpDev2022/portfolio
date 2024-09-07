23d23c3861e514b159ec7c555351a43357473004
LugpDev2022 <lugpdev2022@gmail.com>
2024-09-07 13:02:38 -0600

    refactor(projects-file): change data structure

--- src/projects.ts
+++ src/projects.ts
@@ -1,54 +1,80 @@
 export interface Project {
   title: string;
   image: string;
-  description: string;
+  description: {
+    en: string;
+    es: string;
+  };
   techStack: string[];
   url: string;
 }
 
 export const projects: Project[] = [
+  // {
+  //   title: 'Nextflix',
+  //   image: '/images/projects/nextflix.png',
+  //   description:
+  //     'This project is a Netflix Clone built for educational purposes.',
+  //   techStack: [],
+  //   url: 'https://nextflix-lugpdev.vercel.app/en/landing',
+  // },
   {
     title: 'Todo App',
     image: '/images/projects/todoApp.png',
-    description:
-      'Web application designed for organizing your daily tasks effectively.',
+    description: {
+      en: 'Web application designed for organizing your daily tasks effectively.',
+      es: 'Aplicación web diseñada para organizar tus tareas diarias de manera efectiva',
+    },
     techStack: ['React', 'Bootstrap'],
     url: 'https://lugp-todo-app.netlify.app',
   },
   {
     title: 'Calculator',
     image: '/images/projects/calculator.png',
-    description:
-      'Web calculator for helping you with mathematical operations. It has dark and light mode.',
+    description: {
+      en: 'Web calculator for helping you with mathematical operations. It has dark and light mode.',
+      es: 'Calculadora web para ayudarte con operaciones matemáticas. Cuenta con un modo oscuro y uno claro.',
+    },
     techStack: ['React', 'Typescript', 'Tailwind', 'Vitest'],
     url: 'https://lugp-calculator.netlify.app/',
   },
   {
     title: 'Advice Generator',
     image: '/images/projects/adviceGenerator.png',
-    description: 'Get some of the best advices with this web application.',
+    description: {
+      en: 'Get some of the best advices with this web application.',
+      es: 'Accede a los mejores consejos con esta aplicación web.',
+    },
     techStack: ['React', 'Bootstrap'],
     url: 'https://advice-generator-lugpdev2022.netlify.app/',
   },
   {
     title: 'Time App',
     image: '/images/projects/timeApp.png',
-    description:
-      'Small app for basic time tasks like watching the hour and creating timers.',
+    description: {
+      en: 'Small app for basic time tasks like watching the hour and creating timers.',
+      es: 'Aplicación sencilla para tareas básicas relacionadas con el tiempo, como ver la hora y crear temporizadores.',
+    },
     techStack: ['React', 'Bootstrap', 'Redux', 'Jest'],
     url: 'https://time-app-lugpdev2022.netlify.app/',
   },
   {
     title: 'News Page',
     image: '/images/projects/news-page.png',
-    description: 'Homepage of a news website.',
+    description: {
+      en: 'Homepage of a news website.',
+      es: 'Pagina de inicio de un sitio web de noticias.',
+    },
     techStack: ['Astro', 'Bootstrap', 'Sass'],
     url: 'https://news-page-lugpdev2022.netlify.app/',
   },
   {
     title: '404 Not Found',
     image: '/images/projects/not-found.png',
-    description: 'Typical 404 Not Found page.',
+    description: {
+      en: 'Typical 404 Not Found page.',
+      es: 'Página típica de error 404 No Encontrado',
+    },
     techStack: ['Astro', 'Tailwind', 'Css'],
     url: 'https://lugp-not-found.netlify.app/',
   },
