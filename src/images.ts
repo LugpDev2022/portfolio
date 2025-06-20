7499f6cb83a53c75c6a6481bf783f0c6c753ba75
LugpDev2022 <LugpDev2022@gmail.com>
2025-06-20 12:42:29 -0600

    refactor: update Skillset component to use Skill type and integrate icons for languages

--- src/images.ts
+++ src/images.ts
@@ -1,9 +1,42 @@
+import {
+  SiTypescript,
+  SiJavascript,
+  SiHtml5,
+  SiCss3,
+  SiSass,
+} from 'react-icons/si';
+
+import type { Skill } from '@/components/Skillset.astro';
+
 export const languages: string[] = [
   'html',
   'javascript',
   'css',
   'typescript',
   'sass',
+];
+
+export const languagesWithIcons: Skill[] = [
+  {
+    name: 'html',
+    icon: SiHtml5,
+  },
+  {
+    name: 'javascript',
+    icon: SiJavascript,
+  },
+  {
+    name: 'css',
+    icon: SiCss3,
+  },
+  {
+    name: 'typescript',
+    icon: SiTypescript,
+  },
+  {
+    name: 'sass',
+    icon: SiSass,
+  },
 ];
 
 export const frameworks: string[] = [
